a25edc4c8aab5f9cefee37997f16cfa22d35e8d1
mengliYuan <1600708467@qq.com>
2025-01-17 14:46:39 +0800

    数组:螺旋矩阵

--- src/main/java/Array/LeetCode54.java
+++ src/main/java/Array/LeetCode54.java
@@ -9,6 +9,43 @@ import java.util.List;
 public class LeetCode54 {
 
     //关键点控制循环不变量：左闭右开
+    public List<Integer> spiralOrderWithCarl(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        int numEle = matrix.length * matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0,right = n-1;
+        int top = 0,bottom = m-1;
+        while (numEle>0){
+            //从左到右
+            for(int i = left;i<=right;i++){
+                ans.add(matrix[top][i]);
+                numEle--;
+            }
+            top++;
+            //从上到下
+            for(int i = top;i<=bottom;i++){
+                ans.add(matrix[i][right]);
+                numEle--;
+            }
+            right--;
+            //从右到左
+            for(int i = right;i>=left;i--){
+                ans.add(matrix[bottom][i]);
+                numEle--;
+            }
+            bottom--;
+            //从下到上
+            for(int i = bottom;i>=top;i--){
+                ans.add(matrix[i][left]);
+                numEle--;
+            }
+            left++;
+        }
+        return ans;
+    }
+
+
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> ans = new ArrayList<>();
         int m = matrix.length;
@@ -50,7 +87,9 @@ public class LeetCode54 {
         int[][] matrix = {{3},{2}};
         LeetCode54 leetCode54 = new LeetCode54();
         List<Integer> integers = leetCode54.spiralOrder(matrix);
+        List<Integer> integers1 = leetCode54.spiralOrderWithCarl(matrix);
         System.out.println(integers);
+        System.out.println(integers1);
 
         int[][] matrix2 = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
         List<Integer> integers2 = leetCode54.spiralOrder(matrix2);
--- src/main/java/Array/LeetCode59.java
+++ src/main/java/Array/LeetCode59.java
@@ -3,8 +3,50 @@ package Array;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 59.螺旋矩阵2
+ */
 public class LeetCode59 {
-//    public int[][] generateMatrix(int n) {
-//
-//    }
+    public int[][] generateMatrix(int n) {
+        int[][] ans = new int[n][n];
+        //int numEle = n * n;
+        int left = 0,right = n-1;
+        int top = 0,bottom = n-1;
+        int count = 1;
+        while (count<=n*n){
+            //从左到右
+            for(int i = left;i<=right;i++){
+                ans[top][i] = count++;
+            }
+            top++;
+            //从上到下
+            for(int i = top;i<=bottom;i++){
+                ans[i][right] = count++;
+            }
+            right--;
+            //从右到左
+            for(int i = right;i>=left;i--){
+                ans[bottom][i] = count++;
+            }
+            bottom--;
+            //从下到上
+            for(int i = bottom;i>=top;i--){
+                ans[i][left] = count++;
+            }
+            left++;
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        LeetCode59 leetCode59 = new LeetCode59();
+        int[][] ans = leetCode59.generateMatrix(1);
+        for(int i = 0;i<ans.length;i++){
+            for(int j = 0;j<ans[0].length;j++){
+                System.out.print(ans[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
